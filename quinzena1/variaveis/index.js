80e0caf33c323327cd05dcf77f466f34960ab7fa
julio-san <juliocesra.amancio@yahoo.com.br>
2021-05-20 20:06:10 -0300

    primeiro commit

---
+++ quinzena1/variaveis/index.js
@@ -0,0 +1,64 @@
+// let nome = ""
+// let idade =
+
+// console.log(typeof nome,idade)
+
+//  let nome sem valor declarado, apenas com as aspas, apresentou valor de string, mas a var idade apresentou o valor de undefined, ou seja, valor indefinido " variável sem valor"
+
+// let nome = prompt("Qual o seu nome ?")
+// let idade = prompt("Qual a sua idade ?")
+
+// console.log(typeof nome,idade)
+
+//  os valores digitados em prompt são tratados como string
+
+// console.log("Olá",nome,"você tem",idade,"anos.")
+
+
+
+
+
+
+// //2-
+
+// let roupaAzul = prompt("Você usou roupa azul hoje ?")
+// let copoDagua = prompt("você bebeu agua hoje ?")
+// let soneca = prompt("você dormiu hoje ?")
+
+// // incluso a letra R referente a respostas
+// let roupaAzulr = roupaAzul
+// let copoDaguar = copoDagua
+// let sonecar = soneca
+
+
+
+// console.log("Você usou roupa azul hoje ?", roupaAzulr)
+// console.log("você bebeu agua hoje ?", copoDaguar)
+// console.log("você dormiu hoje ?", sonecar)
+
+
+// -3 
+
+// let a = 10
+// let b = 25
+
+// let c = 10
+
+// a = b
+// b = c 
+ 
+
+// console.log(a,b)
+
+
+
+//DESAFIO
+
+let primeiroNumero = Number (prompt(" Digite um Número"))
+let segundoNumero = Number  (prompt("Digite outro Número"))
+
+let soma = primeiroNumero + segundoNumero
+let mult = primeiroNumero * segundoNumero
+
+console.log("A soma dos dois é =", soma)
+console.log("A multiplicação dos números é =", mult)
