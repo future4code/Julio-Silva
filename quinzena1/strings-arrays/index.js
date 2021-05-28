09ab6664c708cf518b772aecb29cce635eb1d049
julio-san <juliocesra.amancio@yahoo.com.br>
2021-05-27 22:49:20 -0300

    Exercícios de metodos e arrays

---
+++ quinzena1/strings-arrays/index.js
@@ -0,0 +1,70 @@
+ // Exercício 1 interpretação
+
+//  console.log('a.', array) = indefinido
+
+// console.log('b. ', array) = nulla
+
+// console.log('c. ', array.lenght) = c. 10
+
+// console.log('d. ', array[i]) = d. 0
+
+// console.log('e. ', array) = e. 0, 1, 2 até 18
+
+// console.log('f. ', array) = f. 0, 6
+
+
+
+// Exercício 2 interpretação
+
+// a frase do input, será maiuscula, trocará a letra A por I e retornará o tamanho da frase .
+// console.log(SUBI NUM ÔNIBUS EM MIRRACOS, )
+
+
+// Exercício de Escrita 
+
+//   1. 
+
+ const emailDoUsuario = prompt("Digite seu e-mail!")
+const nomeDoUsuario = prompt("Digite seu nome!")
+
+console.log(`O e-mail ${emailDoUsuario} foi cadastrado com sucesso. Seja Bem vinda(o), ${nomeDoUsuario}!`)
+
+
+
+
+// 2.
+
+ let comidas = ["lasanha","empanado","strogonoff","pizza","tapioca"]
+console.log(comidas)
+console.log("Essas são as minhas comidas preferidas:",comidas)
+console.log("Essas são as minhas comidas preferidas:",comidas[0])
+console.log("Essas são as minhas comidas preferidas:",comidas[1])
+console.log("Essas são as minhas comidas preferidas:",comidas[2])
+console.log("Essas são as minhas comidas preferidas:",comidas[3])
+console.log("Essas são as minhas comidas preferidas:",comidas[4])
+
+let comidaUsuario = prompt("Digite sua comida preferida!")
+comidas[2] = comidaUsuario
+console.log("Essas são as minhas comidas preferidas:",comidas)
+console.log("Essas são as minhas comidas preferidas:",comidas[0])
+console.log("Essas são as minhas comidas preferidas:",comidas[1])
+console.log("Essas são as minhas comidas preferidas:",comidas[2])
+console.log("Essas são as minhas comidas preferidas:",comidas[3])
+console.log("Essas são as minhas comidas preferidas:",comidas[4]) 
+
+
+/// 3.
+
+
+let listaDeTarefas = []
+ listaDeTarefas.push(prompt("Digite a primeira tarefa"))
+ listaDeTarefas.push(prompt("Digite a segunda tarefa"))
+ listaDeTarefas.push(prompt("Digite a terceira tarefa"))
+console.log(listaDeTarefas)
+console.log(listaDeTarefas.length) 
+
+
+
+/*    ---------desafio---------
+
+ fica pra proxima... o cansaço dominou  */
