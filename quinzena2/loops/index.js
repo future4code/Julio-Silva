13544e77038058c7fc0312d7203346511e17d562
julio-san <juliocesra.amancio@yahoo.com.br>
2021-06-10 21:50:45 -0300

    Eentrega dos exercícios Loops

---
+++ quinzena2/loops/index.js
@@ -0,0 +1,71 @@
+// Exercícios de interpretação de código
+// 1.
+// no console.log(valor) o valor será de 10, por o valor da variavel valor esta somado ao valor da variavél i.
+
+// 2.
+// a) console.log(numero) apresentará o valor de 19 até o 30, pois o if pede numeros da array maior que 18.
+// b) só o for of é sufuciente, mas se nao fosse poderiamos utilizar o loop for chamando o indice por [i]
+
+// 3.
+// Pelo que vi rodando o código no PYTUTOR, ele vai verificar se existe uma linha vazia e adicionar um asterisco, e essa verificação vai rodar adicionando o numero de asteriscos acompanhando os numeros de linhas.
+
+// Exercícios de escrita de código
+// 1.
+// A,B E C)
+
+/* let bichinhos = Number(prompt("Quantos bichinhos você tem ?"));
+let pets = "";
+let petsUsuario = [];
+
+if (bichinhos === 0) {
+  console.log("Que pena! Você pode adotar um pet!");
+} else {
+  for (let i = 0; i < bichinhos; i++)
+    pets = prompt("Qual o nome dos bichinhos ?");
+  petsUsuario.push(pets);
+}
+console.log([pets]);
+
+// 2. */
+// a)
+/* let original = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55] 
+function imprimir (arrayOriginal){
+    for (let i = 0; i < arrayOriginal.length; i++){
+        console.log(arrayOriginal[i])
+    }
+}
+
+imprimir(original) */
+
+// b)
+/* let original = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55];
+function imprimir(arrayOriginal) {
+  for (let i = 0; i < arrayOriginal.length; i++) {
+    console.log(arrayOriginal[i] / 10);
+  }
+}
+
+imprimir(original)
+ */
+
+/* 
+// c)
+ const arrayPar = [ ]
+
+let original = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55];
+function imprimir(arrayOriginal) {
+  for (let i = 0; i < arrayOriginal.length; i++) {
+    if (arrayOriginal[i] %2 === 0)  {
+        arrayPar.push(arrayOriginal[i])
+        console.log(arrayPar)
+    }
+  }
+  return arrayPar
+}
+
+
+imprimir()
+
+ */
+
+// D)
