f0e73ace97e845608fbc2d3880be69ac8c497b09
juliosan-bit <juliocesar.amancio@yahoo.com.br>
2021-08-15 18:52:43 -0300

    retirando borda

--- quinzena7/labeuser/labenusers/src/App.js
+++ quinzena7/labeuser/labenusers/src/App.js
@@ -1,49 +1,47 @@
-import React from 'react'
-import TelaAdicionar from './components/TelaAdicionar'
-import TelaListaUsuarios from './components/TelaListaUsuarios'
-import styled from 'styled-components'
+import React from "react";
+import TelaAdicionar from "./components/TelaAdicionar";
+import TelaListaUsuarios from "./components/TelaListaUsuarios";
+import styled from "styled-components";
 
 const Pagina = styled.div`
-border-radius: 8px;
-width: 50vw;
-min-height: 50vh;
-display: flex;
-margin: 0 auto;
-justify-content: center;
-border: 1px solid black;
-h1{
-  margin: 8vh;
-}
-input{
-  margin-bottom: 16px;
-}
-button{
-  margin: 16px 0;
-}
-`
-
+  border-radius: 8px;
+  width: 50vw;
+  min-height: 50vh;
+  display: flex;
+  margin: 0 auto;
+  justify-content: center;
+  h1 {
+    margin: 8vh;
+  }
+  input {
+    margin-bottom: 16px;
+  }
+  button {
+    margin: 16px 0;
+  }
+`;
 
 export default class App extends React.Component {
   state = {
     adicionarUsuario: true,
     usuarioVerMais: "",
     telaVerListaUsuarios: false,
-  }
+  };
 
   acessaTelaNovoUsuario = () => {
     this.setState({
       adicionarUsuario: true,
       telaVerListaUsuarios: false,
       usuarioVerMais: false,
-    })
-  }
+    });
+  };
 
   acessaListadeUsuarios = () => {
     this.setState({
       adicionarUsuario: false,
       telaVerListaUsuarios: true,
-    })
-  }
+    });
+  };
 
   render() {
     const renderizarPagina = () => {
@@ -54,20 +52,21 @@ export default class App extends React.Component {
               acessaListadeUsuarios={this.acessaListadeUsuarios}
               verMais={this.verMais}
             />
-          </>)
+          </>
+        );
       } else if (this.state.telaVerListaUsuarios) {
-        return <TelaListaUsuarios
-          acessaTelaNovoUsuario={this.acessaTelaNovoUsuario}
-          acessaListadeUsuarios={this.acessaListadeUsuarios}
-          verMais={this.verMais}
-        />
+        return (
+          <TelaListaUsuarios
+            acessaTelaNovoUsuario={this.acessaTelaNovoUsuario}
+            acessaListadeUsuarios={this.acessaListadeUsuarios}
+            verMais={this.verMais}
+          />
+        );
       }
-    }
+    };
     return (
       <div className="App">
-        <Pagina>
-          {renderizarPagina()}
-        </Pagina>
+        <Pagina>{renderizarPagina()}</Pagina>
       </div>
     );
   }
