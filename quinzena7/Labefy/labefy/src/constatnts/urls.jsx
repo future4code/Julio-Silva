7683c5906642128184f47171013ec3d487a36516
julio-silva <juliocesra.amancio@yahoo.com.br>
2021-08-23 18:51:49 -0300

    limpando os arquivos

--- quinzena7/Labefy/labefy/src/constatnts/urls.jsx
+++
@@ -1,8 +0,0 @@
-export const baseURL =
-  "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists";
-
-export const axiosConfig = {
-  headers: {
-    Authorization: "julio-silva-johnson",
-  },
-};
--- quinzena7/Labefy/src/index.js
+++ quinzena7/Labefy/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'
+import App from './App';
 
 
 ReactDOM.render(
@@ -9,5 +9,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
 
