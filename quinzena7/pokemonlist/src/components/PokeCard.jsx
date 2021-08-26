1b38fe2535f05e86b648947995ece3e90adb766c
julio-silva <juliocesra.amancio@yahoo.com.br>
2021-08-26 16:37:01 -0300

    revisando o código e entrega do exercício

--- quinzena7/pokemonlist/src/components/PokeCard.jsx
+++ quinzena7/pokemonlist/src/components/PokeCard.jsx
@@ -2,31 +2,25 @@ import axios from "axios";
 import React from "react";
 import { EstiloInternoCard } from "./estiloPokeCard";
 
-
-import { useState, useEffect, } from "react";
+import { useState, useEffect } from "react";
 
 function PokeCard(props) {
   const [pokemon, setpokemon] = useState({});
-  
 
   useEffect(() => {
-    
     const fetchPokemon = (pokeName) => {
-      console.log("pokename esta aqui ",pokeName)
       axios
         .get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
         .then((response) => {
           const pokemon = response.data;
-        
+
           setpokemon(pokemon);
-      
-          console.log(response.data);
         })
         .catch((err) => {
           console.log(err, "deu erro");
         });
     };
-    
+
     fetchPokemon(props.pokemon);
   }, [props.pokemon]);
 
